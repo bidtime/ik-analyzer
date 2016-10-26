83a5881c2f67e22f49600605aa6d909555ceda9b
bidtime <bidtime@126.com>
2016-10-26 14:40:51 +0800

    support lucene5.x above

--- src/main/java/org/wltea/analyzer/cfg/Configuration.java
+++ src/main/java/org/wltea/analyzer/cfg/Configuration.java
@@ -1,120 +1,79 @@
 /**
+ * IK 中文分词  版本 5.0
+ * IK Analyzer release 5.0
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * 源代码由林良益(linliangyi2005@gmail.com)提供
+ * 版权声明 2012，乌龙茶工作室
+ * provided by Linliangyi and copyright 2012 by Oolong studio
  * 
  */
 package org.wltea.analyzer.cfg;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.InvalidPropertiesFormatException;
 import java.util.List;
-import java.util.Properties;
-
-import org.wltea.analyzer.dic.Dictionary;
-import org.wltea.analyzer.seg.CJKSegmenter;
-import org.wltea.analyzer.seg.ISegmenter;
-import org.wltea.analyzer.seg.LetterSegmenter;
-import org.wltea.analyzer.seg.QuantifierSegmenter;
 
 /**
- * IK Analyzer v3.2
- * 简单的配置管理类,单子模式
- * @author 林良益
- *
+ * 
+ * 配置管理类接口
+ * 
  */
-public class Configuration {
-	/*
-	 * 分词器配置文件路径
-	 */	
-	private static final String FILE_NAME = "/IKAnalyzer.cfg.xml";
-	//配置属性——扩展字典
-	private static final String EXT_DICT = "ext_dict";
-	//配置属性——扩展停止词典
-	private static final String EXT_STOP = "ext_stopwords";
+public interface Configuration {
 	
-	private static final Configuration CFG = new Configuration();
 	
-	private Properties props;
 	
-	/*
-	 * 初始化配置文件
+	/**
+	 * 返回useSmart标志位
+	 * useSmart =true ，分词器使用智能切分策略， =false则使用细粒度切分
+	 * @return useSmart
 	 */
-	private Configuration(){
-		
-		props = new Properties();
-		
-		InputStream input = Configuration.class.getResourceAsStream(FILE_NAME);
-		if(input != null){
-			try {
-				props.loadFromXML(input);
-			} catch (InvalidPropertiesFormatException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+	public boolean useSmart();
 	
+	/**
+	 * 设置useSmart标志位
+	 * useSmart =true ，分词器使用智能切分策略， =false则使用细粒度切分
+	 * @param useSmart
+	 */
+	public void setUseSmart(boolean useSmart);
+	
+	
+	/**
+	 * 获取主词典路径
+	 * 
+	 * @return String 主词典路径
+	 */
+	public String getMainDictionary();
+
+	/**
+	 * 获取量词词典路径
+	 * @return String 量词词典路径
+	 */
+	public String getQuantifierDicionary();
+
 	/**
 	 * 获取扩展字典配置路径
 	 * @return List<String> 相对类加载器的路径
 	 */
-	public static List<String> getExtDictionarys(){
-		List<String> extDictFiles = new ArrayList<String>(2);
-		String extDictCfg = CFG.props.getProperty(EXT_DICT);
-		if(extDictCfg != null){
-			//使用;分割多个扩展字典配置
-			String[] filePaths = extDictCfg.split(";");
-			if(filePaths != null){
-				for(String filePath : filePaths){
-					if(filePath != null && !"".equals(filePath.trim())){
-						extDictFiles.add(filePath.trim());
-						//System.out.println(filePath.trim());
-					}
-				}
-			}
-		}		
-		return extDictFiles;		
-	}
-	
+	public List<String> getExtDictionarys();
+
+
 	/**
 	 * 获取扩展停止词典配置路径
 	 * @return List<String> 相对类加载器的路径
 	 */
-	public static List<String> getExtStopWordDictionarys(){
-		List<String> extStopWordDictFiles = new ArrayList<String>(2);
-		String extStopWordDictCfg = CFG.props.getProperty(EXT_STOP);
-		if(extStopWordDictCfg != null){
-			//使用;分割多个扩展字典配置
-			String[] filePaths = extStopWordDictCfg.split(";");
-			if(filePaths != null){
-				for(String filePath : filePaths){
-					if(filePath != null && !"".equals(filePath.trim())){
-						extStopWordDictFiles.add(filePath.trim());
-						//System.out.println(filePath.trim());
-					}
-				}
-			}
-		}		
-		return extStopWordDictFiles;		
-	}
-		
-	
-	/**
-	 * 初始化子分词器实现
-	 * （目前暂时不考虑配置扩展）
-	 * @return List<ISegmenter>
-	 */
-	public static List<ISegmenter> loadSegmenter(){
-		//初始化词典单例
-		Dictionary.getInstance();
-		List<ISegmenter> segmenters = new ArrayList<ISegmenter>(4);
-		//处理数量词的子分词器
-		segmenters.add(new QuantifierSegmenter());
-		//处理中文词的子分词器
-		segmenters.add(new CJKSegmenter());
-		//处理字母的子分词器
-		segmenters.add(new LetterSegmenter()); 
-		return segmenters;
-	}
+	public List<String> getExtStopWordDictionarys();
+			
 }
